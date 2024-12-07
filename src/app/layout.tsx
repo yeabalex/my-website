96c4f03a4f1eb9c925342653e1cf61ca3d8c58aa
Yeabsira A. <133959744+yeabalex@users.noreply.github.com>
2024-12-07 06:13:39 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9723083264237471"
-     crossorigin="anonymous"></script>
-      </head>
+<head>
+  <script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9723083264237471"
+    crossOrigin="anonymous"
+  ></script>
+</head>
+
       <body
         className={`antialiased`}
       >
